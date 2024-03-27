2f400e4e917f7127caa52f08e370d3106f7ce8e4
Admin <Admin@DESKTOP-HBUKCNC.lan>
2024-03-27 01:31:05 -0400

    Commit

---
+++ demoForJpa/src/main/java/com/example/demo/Service/StudentService.java
@@ -0,0 +1,128 @@
+package com.example.demo.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repo.StudentRepo;
+
+@Service
+public class StudentService {
+		@Autowired
+		StudentRepo studentRepo;
+		
+		public ResponseEntity<Student> saveStudent(@RequestBody Student student)
+		{ 
+			return new ResponseEntity<>(studentRepo.save(student),HttpStatus.CREATED);		
+		}
+		
+		
+		public ResponseEntity<List<Student>> getStudents(String sortBy, String orderBy) 
+		{
+			List<Student> students=studentRepo.findAll();
+			List<Student> sortedStudents=null;
+			if(orderBy.equalsIgnoreCase("ascending"))
+			{
+	        if (sortBy.equalsIgnoreCase("name")) {
+	            sortedStudents = students.stream()
+	                    .sorted(Comparator.comparing(student -> student.getStudentName()))
+	                    .collect(Collectors.toList());
+	        } else if(sortBy.equalsIgnoreCase("id"))
+	            sortedStudents = students.stream()
+	                    .sorted(Comparator.comparing(student -> student.getId()))
+	                    .collect(Collectors.toList());
+	        
+		else
+	        {
+	        	sortedStudents = students.stream()
+	                    .sorted(Comparator.comparing(student -> student.getStudentEmail()))
+	                    .collect(Collectors.toList());
+	        }
+			}
+	        
+	    
+		else if(orderBy.equalsIgnoreCase("descending"))
+		{
+			if (sortBy.equalsIgnoreCase("name")) {
+	            sortedStudents = students.stream()
+	                    .sorted(Comparator.comparing(student -> ((Student) student).getStudentName()).reversed())
+	                    .collect(Collectors.toList());
+	        } else if(sortBy.equalsIgnoreCase("id"))
+	            sortedStudents = students.stream()
+	                    .sorted(Comparator.comparing(student -> ((Student) student).getId()).reversed())
+	                    .collect(Collectors.toList());
+	        
+		else
+	        {
+	        	sortedStudents = students.stream()
+	                    .sorted(Comparator.comparing(student -> ((Student) student).getStudentEmail()).reversed())
+	                    .collect(Collectors.toList());
+	        }
+		}
+			
+		return ResponseEntity.ok(sortedStudents);
+		}	
+			//return new ResponseEntity<>(studentRepo.findAll(),HttpStatus.OK);
+		
+		
+		
+		public ResponseEntity<Student> getStudent(@PathVariable long id) 
+		{
+			Optional<Student> student = studentRepo.findById(id);
+			if(student.isPresent())
+			{
+				return new ResponseEntity<>(student.get(),HttpStatus.OK);
+			}
+			else
+			{
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		}
+		
+		
+		public ResponseEntity<Student> updateStudent(@PathVariable long id, @RequestBody Student stud) 
+		{
+			Optional<Student> student = studentRepo.findById(id);
+			if(student.isPresent())
+			{
+				student.get().setStudentName(stud.getStudentName());
+				student.get().setStudentEmail(stud.getStudentEmail());
+				student.get().setStudentAddress(stud.getStudentAddress());
+				return new ResponseEntity<>(studentRepo.save(student.get()),HttpStatus.OK);
+			}
+			else
+			{
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		}
+		
+		public ResponseEntity<Void> deleteStudent(@PathVariable long id) 
+		{
+			Optional<Student> student = studentRepo.findById(id);
+			if(student.isPresent())
+			{
+				studentRepo.deleteById(id);
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			else
+			{
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		}
+		}
+
+
+		
+
+
---
+++ demoForJpa/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,59 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Service.StudentService;
+import com.example.demo.entity.Student;
+import com.example.demo.repo.StudentRepo;
+
+@RestController
+public class StudentController {
+	@Autowired
+	StudentService studentService;
+	
+	@PostMapping("/api/students")
+	public ResponseEntity<Student> saveStudent(@RequestBody Student student)
+	{ 
+		return studentService.saveStudent(student)	;	
+	}
+	
+	@GetMapping("/api/students")
+	public ResponseEntity<List<Student>> getStudents(@RequestHeader(value = "SortBy", defaultValue = "name") String sortBy, 
+			@RequestHeader(value = "OrderBy", defaultValue = "ascending") String orderBy) 
+	{
+		return studentService.getStudents(sortBy,orderBy);
+			
+	}
+	
+	@GetMapping("/api/students/{id}")
+	public ResponseEntity<Student> getStudent(@PathVariable long id) 
+	{
+		return studentService.getStudent(id);
+	}
+	
+	@PutMapping("/api/students/{id}")
+	public ResponseEntity<Student> updateStudent(@PathVariable long id, @RequestBody Student stud) 
+	{
+		return studentService.updateStudent(id, stud);
+	}
+	@DeleteMapping("/api/students/{id}")
+	public ResponseEntity<Void> deleteStudent(@PathVariable long id) 
+	{
+		return studentService.deleteStudent(id);
+	}
+
+
+}
